004cfcbedb228a910f02171f0f20811210ec8e54
Marlon Salles <marlonsalles@hotmail.com>
2022-05-01 15:50:41 -0300

    Directory & DirectoryInfo

---
+++ Directory_DirectoryInfo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Área de Trabalho\teste";
+            try
+            {
+                var folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories); //Tipo IEnumerable 
+                Console.WriteLine("FOLDERS:");
+                foreach (string s in folders)
+                {
+                    Console.WriteLine(s);
+                }
+                var files = Directory.EnumerateFiles(path, "haha.txt", SearchOption.AllDirectories);
+                Console.WriteLine("FILES:");
+                foreach (string s in files)
+                {
+                    Console.WriteLine(s);
+                }
+                Directory.CreateDirectory(@"C:\Área de Trabalho\NovaPasta");
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
